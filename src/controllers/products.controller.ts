ecd3ed4323ad48848cc83e157a9dc8417563297b
unknown <ohadinho25@gmail.com>
2019-04-25 19:24:30 +0300

    added more operations to products service

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,8 +1,7 @@
 import * as express from "express";
 import {inject} from "inversify";
-import {BaseHttpController, controller, httpGet, HttpResponseMessage, interfaces} from "inversify-express-utils";
+import {BaseHttpController, controller, httpDelete, httpGet, httpPost, httpPut} from "inversify-express-utils";
 import TYPES from "../constants/types";
-import {IProduct} from "../db/models/product.db.model";
 import {ProductsService} from "../services/products.service";
 import {JsonResult} from "inversify-express-utils/dts/results";
 
@@ -15,7 +14,35 @@ export class ProductsController extends BaseHttpController {
     @httpGet("/")
     public async getProducts(req: express.Request, res: express.Response, next: express.NextFunction): Promise<JsonResult> {
         const content = await this.productsService.getProducts();
+        const statusCode = 200;
+        return this.json(content, statusCode);
+    }
+
+    @httpGet("/:id")
+    public async getProduct(req: express.Request, res: express.Response, next: express.NextFunction): Promise<JsonResult> {
+        const content = await this.productsService.getProduct(req.params.id);
         const statusCode = 403;
+        return this.json(content, statusCode);
+    }
+
+    @httpPost("/")
+    public async createProduct(req: express.Request, res: express.Response, next: express.NextFunction): Promise<JsonResult> {
+        const content = await this.productsService.createProduct(req.body);
+        const statusCode = 201;
+        return this.json(content, statusCode);
+    }
+
+    @httpPut("/:id")
+    public async updateProduct(req: express.Request, res: express.Response, next: express.NextFunction): Promise<JsonResult> {
+        const content = await this.productsService.updateProduct(req.params.id, req.body);
+        const statusCode = 200;
+        return this.json(content, statusCode);
+    }
+
+    @httpDelete("/:id")
+    public async deleteProduct(req: express.Request, res: express.Response, next: express.NextFunction): Promise<JsonResult> {
+        const content = await this.productsService.deleteProduct(req.params.id);
+        const statusCode = 200;
         return this.json(content, statusCode);
     }
 }
--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -4,10 +4,50 @@ import Product, {IProduct} from "../db/models/product.db.model";
 
 @injectable()
 export class ProductsService {
-    public async getProducts(): Promise<any> {
+    public async getProducts(): Promise<IProduct[]> {
        return Product.find()
             .then((data: IProduct[]) => {
                 return data;
+            })
+            .catch((error: Error) => {
+                throw error;
+            });
+    }
+
+    public async getProduct(id: number): Promise<IProduct[]> {
+        return Product.find({id})
+            .then((data: IProduct[]) => {
+                return data;
+            })
+            .catch((error: Error) => {
+                throw error;
+            });
+    }
+
+    public async createProduct(product: IProduct): Promise<IProduct> {
+        return Product.create(product)
+            .then((data: IProduct) => {
+                return data;
+            })
+            .catch((error: Error) => {
+                throw error;
+            });
+    }
+
+    public async updateProduct(id: number, product: IProduct): Promise<IProduct> {
+        return Product.findOneAndUpdate({id}, product, {new: true})
+            .then((data: IProduct) => {
+                return data;
+            })
+            .catch((error: Error) => {
+                throw error;
+            });
+    }
+
+    public async deleteProduct(id: number): Promise<boolean> {
+        return Product.findOneAndDelete({id})
+            .then((data: any) => {
+                return true;
             })
             .catch((error: Error) => {
                 throw error;
